67f362a4ce9d70865331fb9ab09f34a5680cbb7c
Gyan <gbhaskar89@gmail.com>
2020-04-08 21:22:06 +0530

    First Commit

---
+++ src/test/java/com/example/testng/TestNGDemo/TestNgDemoApplicationTests.java
@@ -0,0 +1,51 @@
+package com.example.testng.TestNGDemo;
+
+import org.testng.annotations.Test;
+
+import com.example.testng.TestNGDemo.model.Employee;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.web.context.WebApplicationContext;
+
+@SpringBootTest(classes = TestNgDemoApplication.class)
+class TestNgDemoApplicationTests extends AbstractTestNGSpringContextTests {
+	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	private MockMvc mockMvc;
+	
+	
+
+	@BeforeClass
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void testEmployee() throws Exception {
+		mockMvc.perform(get("/employee")).andExpect(status().isOk())
+				.andExpect(content().contentType("application/json"))
+				.andExpect(jsonPath("$.name").value("emp1")).andExpect(jsonPath("$.designation").value("manager"))
+				.andExpect(jsonPath("$.empId").value("1")).andExpect(jsonPath("$.salary").value(3000));
+
+	}
+}
